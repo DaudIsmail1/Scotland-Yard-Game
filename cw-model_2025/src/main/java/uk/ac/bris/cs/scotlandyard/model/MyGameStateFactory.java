57322088266f094836818cb4f46946aa853e7ff6
Daud Ismail <daudismail@eduroam-86-177.nomadic.bris.ac.uk>
2025-03-05 11:10:25 +0000

    Setup and first few tests

--- cw-model_2025/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ cw-model_2025/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -4,21 +4,106 @@ import com.google.common.collect.ImmutableList;
 
 import javax.annotation.Nonnull;
 
+import com.google.common.collect.ImmutableSet;
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import static uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Ticket.DOUBLE;
+import static uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Ticket.TAXI;
 
 /**
  * cw-model
  * Stage 1: Complete this class
  */
 public final class MyGameStateFactory implements Factory<GameState> {
+	private final class MyGameState implements GameState {
+		private GameSetup setup;
+		private ImmutableSet<Piece> remaining;
+		private ImmutableList<LogEntry> log;
+		private Player mrX;
+		private List<Player> detectives;
+		private ImmutableSet<Move> moves;
+		private ImmutableSet<Piece> winner;
+
+		private MyGameState(
+				final GameSetup setup,
+				final ImmutableSet<Piece> remaining,
+				final ImmutableList<LogEntry> log,
+				final Player mrX,
+				final List<Player> detectives)
+		{
+			this.setup = setup;
+			this.remaining = remaining;
+			this.log = log;
+			this.mrX = mrX;
+			this.detectives = detectives;
+			this.moves = ImmutableSet.of();
+			this.winner = ImmutableSet.of();
+
+
+			if(setup.moves.isEmpty()) throw new IllegalArgumentException("Moves is empty!");
+			if(detectives.isEmpty()) throw new NullPointerException("Detectives is empty!");
+			if(isDetectivesDouble()) throw new IllegalArgumentException("Detectives must be have double tickets!");
+//			if (setup.graph.isEmpty()) throw new IllegalArgumentException("Graph is empty!");
+//
+
+			
+		}
+		public boolean isDetectivesDouble() {
+			for (Player player : detectives) {
+				if(player.has(DOUBLE)) return true;
+			}
+			return false;
+		}
+
+
+		@Override public GameSetup getSetup() {  return null; }
+
+		@Override  public ImmutableSet<Piece> getPlayers() { return null; }
+
+		@Nonnull
+		@Override
+		public Optional<Integer> getDetectiveLocation(Piece.Detective detective) {
+			return Optional.empty();
+		}
+
+		@Nonnull
+		@Override
+		public Optional<TicketBoard> getPlayerTickets(Piece piece) {
+			return Optional.empty();
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableList<LogEntry> getMrXTravelLog() {
+			return null;
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableSet<Piece> getWinner() {
+			return ImmutableSet.of();
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableSet<Move> getAvailableMoves() {
+			return null;
+		}
+
+		@Override public GameState advance(Move move) {  return null;  }
+	}
+
 
 	@Nonnull @Override public GameState build(
 			GameSetup setup,
 			Player mrX,
 			ImmutableList<Player> detectives) {
-		// TODO
-		throw new RuntimeException("Hello");
+		return new MyGameState(setup, ImmutableSet.of(Piece.MrX.MRX), ImmutableList.of(), mrX, detectives);
 
 	}
 
