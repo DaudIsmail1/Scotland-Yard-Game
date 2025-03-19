6b4cae618e50f01b02721be491a3f79fcb441ed9
Daud Ismail <daudismail@eduroam-193-174.nomadic.bris.ac.uk>
2025-03-19 11:48:05 +0000

    Observer began

--- cw-model_2025/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
+++ cw-model_2025/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
@@ -4,7 +4,11 @@ import com.google.common.collect.ImmutableList;
 
 import javax.annotation.Nonnull;
 
+import com.google.common.collect.ImmutableSet;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * cw-model
@@ -12,10 +16,43 @@ import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
  */
 public final class MyModelFactory implements Factory<Model> {
 
+
+
 	@Nonnull @Override public Model build(GameSetup setup,
 	                                      Player mrX,
 	                                      ImmutableList<Player> detectives) {
 		// TODO
-		throw new RuntimeException("Implement me!");
+		class MyModel implements Model{
+
+			@Nonnull
+			@Override
+			public Board getCurrentBoard() {
+				return null;
+			}
+
+			@Override
+			public void registerObserver(@Nonnull Observer observer) {
+
+			}
+
+			@Override
+			public void unregisterObserver(@Nonnull Observer observer) {
+
+			}
+
+			@Nonnull
+			@Override
+			public ImmutableSet<Observer> getObservers() {
+				return null;
+			}
+
+			@Override
+			public void chooseMove(@Nonnull Move move) {
+				// TODO Advance the model with move, then notify all observers of what what just happened.
+				//  you may want to use getWinner() to determine whether to send out Event.MOVE_MADE or Event.GAME_OVER
+
+			}
+		}
+		return new MyModel();
 	}
 }
